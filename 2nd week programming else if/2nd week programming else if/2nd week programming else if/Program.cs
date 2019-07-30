82944d561bed19f19b992fe253a609e0ab37b2f0
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-31 11:41:51 +1200

    commit 1

---
+++ 2nd week programming else if/2nd week programming else if/2nd week programming else if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_week_programming_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 10: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1==4)
+            { Console.WriteLine("Congratulations you have guessed the correct number!");
+            }
+
+            else
+            { Console.WriteLine("You lose :("); }
+            Console.ReadLine();
+            
+        }
+    }
+}
